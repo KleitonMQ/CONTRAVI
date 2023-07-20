958d26ad79a10d0050f06303ba6a2c77c35ab914
Kleiton MQ <kleiton.x5@gmail.com>
2023-07-20 20:01:24 -0300

    start do projeto, configuração do git ignore

---
+++ CONTRAVI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CONTRAVI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
