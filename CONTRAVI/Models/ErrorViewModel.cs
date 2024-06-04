6d1fc9a860886a6dbe9a378573f86080d8b04afc
Kleiton MQ <kleiton.x5@gmail.com>
2024-06-04 10:57:16 -0300

    prototipacao das paginas

--- CONTRAVI/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace CONTRAVI.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
