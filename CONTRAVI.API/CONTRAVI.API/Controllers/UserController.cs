390507bc92d1df2c2bf6bbd14a0562643c1f4908
KleitonMQ <kleiton.x5@gmail.com>
2025-04-18 13:15:26 -0300

    add: camada application, cdd controlers

---
+++ CONTRAVI.API/CONTRAVI.API/Controllers/UserController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CONTRAVI.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        [HttpPost]
+        public async Task<IActionResult> PostPassenger()
+        {
+            //Incluir metodo
+            return Ok();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> PostAdmin()
+        {
+            //Incluir metodo
+            return Ok();
+        }
+        [HttpPost]
+        public async Task<IActionResult> PostDriver()
+        {
+            //Incluir metodo
+            return Ok();
+        }
+
+
+    }
+}
